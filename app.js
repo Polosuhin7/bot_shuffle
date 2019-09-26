7ebdd6f6ed276f11f30c6e09ac5054cf0c8e2521
Vasiliy <polosuhin7@gmail.com>
2019-09-27 00:45:44 +0300

    'wip'

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const bot = new Telegraf(token)
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there1'))
 bot.launch()
 
 bot.use(async (ctx, next) => {
