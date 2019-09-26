36c77c018f75255fe5e2394c087778a15495be4e
Vasiliy <polosuhin7@gmail.com>
2019-09-27 00:05:15 +0300

    'wip'

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const Telegraf = require('telegraf');
 const token = '847696441:AAGMEBOF5rKEYUaeGIy-e1gAOvbKi3UlQ_k';
 
 const bot = new Telegraf(token)
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
-bot.command('modern', ({ reply }) => reply('Yo'))
-bot.command('hipster', Telegraf.reply('λ'))
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
 
 bot.use(async (ctx, next) => {
